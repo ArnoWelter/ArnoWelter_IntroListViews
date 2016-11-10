2af84665d7415d364cd190e6fd43c92fe95ec6a7
arnow_000 <arnowelter03@gmail.com>
2016-11-10 15:44:25 +0100

    Actividad_3_8

---
+++ app/src/main/java/comunicacionactivities/example/net/introduccionalistviews/MainActivity.java
@@ -0,0 +1,13 @@
+package comunicacionactivities.example.net.introduccionalistviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
